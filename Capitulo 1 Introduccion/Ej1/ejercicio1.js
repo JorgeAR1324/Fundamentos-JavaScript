70d9ad0a1c13b6235c4fb9290a7774e4cd9f8309
JorgeAR1324 <jorgeramirez01006@gmail.com>
2026-05-28 20:12:25 -0500

    Primera version

---
+++ Capitulo 1 Introduccion/Ej1/ejercicio1.js
@@ -0,0 +1,10 @@
+/* Esto sirve para realizar comentarios en el 
+codigo, es decir, paa escribir texto que no se 
+ejecutara como codigo.
+los comentarios son utiles para expicar el proposito 
+de ciertas partes del codigo, para dejar notas para 
+otros desaroladores o para recorar algo importante 
+sobre el codigo en el futuro
+*/
+document.write("Hola Mundo")
+console.log("Hola desde la consola")
